d41dabcfc58901af20f45f2d8535728e2c871164
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-21 22:16:32 -0300

    feat: criar entidade Tarefa com Room e Parcelize

--- app/src/main/java/com/example/personaltasks/model/Tarefa.kt
+++ app/src/main/java/com/example/personaltasks/model/Tarefa.kt
@@ -1,4 +1,25 @@
 package com.example.personaltasks.model
 
-class Tarefa {
-}
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import java.time.LocalDate
+
+/**
+ * Entidade que representa uma Tarefa no banco de dados.
+ * @Entity define que essa classe sera uma tabela.
+ * @Parcelize permite que possamos enviar a Tarefa entre Activities.
+ */
+
+@Parcelize
+@Entity(tableName = "tarefas")
+data class Tarefa(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0, // ID autogerado pelo Room
+
+    val titulo: String,
+    val descricao: String,
+    val dataLimite: LocalDate
+
+) : Parcelable
