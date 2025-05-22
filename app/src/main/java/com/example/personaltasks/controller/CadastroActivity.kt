51d4fb10ef4c378dc51e30dbd76ea70df5035343
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-22 19:27:46 -0300

    feat: adicionar função configurarDatePicker na CadastroActivity

--- app/src/main/java/com/example/personaltasks/controller/CadastroActivity.kt
+++ app/src/main/java/com/example/personaltasks/controller/CadastroActivity.kt
@@ -1,10 +1,13 @@
 package com.example.personaltasks.controller
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.personaltasks.databinding.ActivityCadastroBinding
 import com.example.personaltasks.model.Tarefa
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class CadastroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCadastroBinding
@@ -16,6 +19,24 @@ class CadastroActivity : AppCompatActivity() {
         // Inicializa o ViewBinding
         binding = ActivityCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun configurarDatePicker() {
+        binding.etDataLimite.setOnClickListener {
+            val calendario = Calendar.getInstance()
+            val ano = calendario.get(Calendar.YEAR)
+            val mes = calendario.get(Calendar.MONTH)
+            val dia = calendario.get(Calendar.DAY_OF_MONTH)
+
+            val datePicker = DatePickerDialog(this, { _, year, month, dayOfMonth ->
+                dataSelecionada = LocalDate.of(year, month + 1, dayOfMonth)
+                binding.etDataLimite.setText(
+                    dataSelecionada?.format(DateTimeFormatter.ISO_LOCAL_DATE)
+                )
+            }, ano, mes, dia)
+
+            datePicker.show()
+        }
     }
 
 }
