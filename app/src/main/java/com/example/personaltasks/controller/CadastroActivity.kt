b45a3c7f71c44faaf35e4d13b69b7bb41ac266c6
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-22 19:23:33 -0300

    feat: configurar ViewBinding na CadastroActivity

--- app/src/main/java/com/example/personaltasks/controller/CadastroActivity.kt
+++ app/src/main/java/com/example/personaltasks/controller/CadastroActivity.kt
@@ -1,5 +1,6 @@
 package com.example.personaltasks.controller
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.personaltasks.databinding.ActivityCadastroBinding
 import com.example.personaltasks.model.Tarefa
@@ -8,4 +9,13 @@ import java.time.LocalDate
 class CadastroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCadastroBinding
     private var dataSelecionada: LocalDate? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inicializa o ViewBinding
+        binding = ActivityCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
 }
