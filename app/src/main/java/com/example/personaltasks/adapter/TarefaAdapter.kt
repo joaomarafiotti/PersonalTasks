e7548a948ffed384670251536ce37b1b25587328
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-22 18:35:59 -0300

    feat: criar ViewHolder no TarefaAdapter

--- app/src/main/java/com/example/personaltasks/adapter/TarefaAdapter.kt
+++ app/src/main/java/com/example/personaltasks/adapter/TarefaAdapter.kt
@@ -1,4 +1,17 @@
 package com.example.personaltasks.adapter
 
-class TarefaAdapter {
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.personaltasks.R
+
+/**
+ * ViewHolder para representar cada item da lista de tarefas.
+ */
+
+class TarefaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val tvTitulo: TextView = itemView.findViewById(R.id.tvTitulo)
+    val tvDescricao: TextView = itemView.findViewById(R.id.tvDescricao)
+    val tvDataLimite: TextView = itemView.findViewById(R.id.tvDataLimite)
 }
