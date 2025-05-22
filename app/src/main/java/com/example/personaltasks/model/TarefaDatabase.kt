4a32b25be29a4d2e4ba3cb6a4698db7bbdf5682e
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-21 22:50:57 -0300

    feat: adicionar TypeConverter (LocalDateConverter) na TarefaDatabase

--- app/src/main/java/com/example/personaltasks/model/TarefaDatabase.kt
+++ app/src/main/java/com/example/personaltasks/model/TarefaDatabase.kt
@@ -2,12 +2,14 @@ package com.example.personaltasks.model
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 /**
  * Classe de configuração do banco de dados Room.
  * Define as entidades e os conversores utilizados.
  */
 @Database(entities = [Tarefa::class], version = 1)
+@TypeConverters(LocalDateConverter::class)
 abstract class TarefaDatabase : RoomDatabase() {
 
     /**
