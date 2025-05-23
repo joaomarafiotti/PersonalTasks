3f1f6ebb2ab89d951dcc080be2d2e113d6330f5b
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-22 21:44:55 -0300

    chore: adicionar variáveis do RecyclerView na MainActivity

--- app/src/main/java/com/example/personaltasks/controller/MainActivity.kt
+++ app/src/main/java/com/example/personaltasks/controller/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.personaltasks.controller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.personaltasks.adapter.TarefaAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: TarefaAdapter
 }
