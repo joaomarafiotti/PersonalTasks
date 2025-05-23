71ff2bb0c0997a17e9748b090ae00b04610e017e
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-22 21:48:04 -0300

    feat: implementar função para carregar lista de tarefas na MainActivity

--- app/src/main/java/com/example/personaltasks/controller/MainActivity.kt
+++ app/src/main/java/com/example/personaltasks/controller/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.personaltasks.R
 import com.example.personaltasks.adapter.TarefaAdapter
+import com.example.personaltasks.model.Tarefa
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,15 @@ class MainActivity : AppCompatActivity() {
         adapter = TarefaAdapter(listOf()) // inicialmente lista vazia
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = TarefaAdapter(carregarTarefas())
     }
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: TarefaAdapter
+
+    private fun carregarTarefas(): List<Tarefa> {
+        return listOf(
+            Tarefa(titulo = "Estudar", descricao = "Revisar Kotlin", dataLimite = LocalDate.now().plusDays(2)),
+            Tarefa(titulo = "Projeto", descricao = "Finalizar App", dataLimite = LocalDate.now().plusDays(5))
+        )
+    }
 }
