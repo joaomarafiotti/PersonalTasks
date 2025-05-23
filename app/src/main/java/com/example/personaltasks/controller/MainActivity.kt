8d7b5d2b163cfd4e6b175ce4b46cb72865660202
joaomarafiotti <joao.marafiotti@aluno.ifsp.edu.br>
2025-05-22 21:46:42 -0300

    feat: configurar RecyclerView na MainActivity

--- app/src/main/java/com/example/personaltasks/controller/MainActivity.kt
+++ app/src/main/java/com/example/personaltasks/controller/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.personaltasks.controller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.personaltasks.R
 import com.example.personaltasks.adapter.TarefaAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        recyclerView = findViewById(R.id.rvTarefas)
+        adapter = TarefaAdapter(listOf()) // inicialmente lista vazia
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: TarefaAdapter
